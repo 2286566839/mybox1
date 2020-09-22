d9365db8cde022834f29c4a70b27c5c2330e88fe
2286566839 <71681507+2286566839@users.noreply.github.com>
2020-09-22 10:32:56 +0800

    Add files via upload

---
+++ 考核加密.py
@@ -0,0 +1,9 @@
+word=input('请输入字母串')
+a=int(input('请输入偏移量'))
+for b in word:
+    if'a'<= b <='z':
+        print(chr(ord('a')+(ord(b)-ord('a')+a)%26),end="")
+    elif'A'<= b <='Z':
+        print(chr(ord('A')+(ord(b)-ord('A')+a)%26),end="")
+    else:
+        print('字母串都不知道是啥么？？？')
